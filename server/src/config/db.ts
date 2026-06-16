23edd9a8d65c55d9205ceda19990772e5ba0e2ed
Shivam Sahray <sahrasyshivam@gmail.com>
2026-06-16 13:12:56 +0530

    fresh

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        console.log("MONGO_URI:", process.env.MONGO_URI);
+        // console.log("MONGO_URI:", process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI!);
 
         console.log("MongoDB Connected");
