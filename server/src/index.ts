5114fa050801ea16429c52d8392d94fb224cdd84
Shivam Sahray <sahrasyshivam@gmail.com>
2026-06-16 13:23:41 +0530

    fresh

--- server/src/index.ts
+++ server/src/index.ts
@@ -69,7 +69,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   // ✅ restoreWhatsAppClients — session check ke saath
-  await restoreWhatsAppClients();
+  // await restoreWhatsAppClients();
 });
 
 
